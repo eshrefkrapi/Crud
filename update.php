eba95f79ecfb69910ea4cf780c375253b8d4d90e
eshrefkrapi <eshref.krapi14@gmail.com>
2023-07-15 10:52:22 +0200

    Fixed bugs

--- update.php
+++ update.php
@@ -1,24 +1,26 @@
 <?php
+
 include 'connect.php';
 $id=$_GET['updatedid'];
-$sql = "select * from `crudtable1` where id='$id'";
-$result= mysqli_query($con,$sql);
+$sql = "Select * from `crudtable1` where id=$id";
+$result=mysqli_query($con,$sql);
 $row=mysqli_fetch_assoc($result);
 $name=$row['name'];
 $email=$row['email'];
 $mobile=$row['mobile'];
 $password=$row['password'];
 
-if(isset($_POST['update'])){
-    $name=$POST['name'];
-    $email=$POST['email'];
-    $mobile=$POST['mob'];
-    $password=$POST['pass'];
+if(isset($_POST['submit'])){
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $mobile=$_POST['mob'];
+    $password=$_POST['pass'];
+
 
     $sql="update `crudtable1` set id=$id, name='$name', email='$email', mobile='$mobile', password='$password' where id=$id";
     $result=mysqli_query($con,$sql);
     if($result){
-       // echo "Data updated successfully";
+       // echo "Data inserted successfully";
        header('location:display.php');
     }else{
         die(mysqli_error($con));
@@ -42,25 +44,25 @@ if(isset($_POST['update'])){
 
   <div class="container my-5">
 
-  <form action="update.php" method="post">
+  <form action="useradd.php" method="post">
   <div class="form-group">
     <label for="name">Name</label>
-    <input type="text" class="form-control" id="name" aria-describedby="emailHelp" placeholder="Enter name" name="name" autocomplete="off" Required value=<?php echo "$name"; ?>>
+    <input type="text" class="form-control" id="name" aria-describedby="emailHelp" placeholder="Enter name" name="name" autocomplete="off" required value=<?php echo $name;?>>
   </div>
   <div class="form-group">
     <label for="email">Email address</label>
-    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" name="email" autocomplete="off" Required value=<?php echo "$email"; ?>>
+    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" name="email" autocomplete="off" Required value=<?php echo $email;?>>
     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
   <div class="form-group">
     <label for="mob">Mobile</label>
-    <input type="number" class="form-control" id="mob"  placeholder="Enter your mobile number" name="mob" autocomplete="off" Required value=<?php echo "$mobile"; ?>>
+    <input type="number" class="form-control" id="mob"  placeholder="Enter your mobile number" name="mob" autocomplete="off" Required value=<?php echo $mobile;?>>
   </div>
   <div class="form-group">
     <label for="pass">Password</label>
-    <input type="password" class="form-control" id="pass" placeholder="Password" name="pass" autocomplete="off" Required value=<?php echo "$password"; ?>>
+    <input type="password" class="form-control" id="pass" placeholder="Password" name="pass" autocomplete="off" Required value=<?php echo $password;?>>
   </div>
-  <button type="submit" class="btn btn-primary" name="update">Update</button>
+  <button type="submit" class="btn btn-primary" name="submit">Update</button>
 </form>
   </div>
   </body>
